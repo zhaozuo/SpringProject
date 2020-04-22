ac6f38f188c640b2d0c26c0baa6b4bcec8a8c481
WZZ <1974077165@qq.com>
2020-04-23 01:05:58 +0800

    使用匿名内部类的方式实现动态代理

--- chapter03/src/main/java/cn/sdut/jdk/JdkProxy.java
+++ chapter03/src/main/java/cn/sdut/jdk/JdkProxy.java
@@ -22,12 +22,36 @@ public class JdkProxy implements InvocationHandler {
         Class[] clazz = userDao.getClass().getInterfaces();
         // 3.使用代理类，进行增强，返回的是代理后的对象
         return Proxy.newProxyInstance(classLoader, clazz, this);
+//        return Proxy.newProxyInstance(JdkProxy.class.getClassLoader(), JdkProxy.class.getInterfaces(),
+//                new InvocationHandler() {
+//                    /**
+//                     * 所有动态代理类的方法调用，都会交由invoke()方法去处理（都会经过该方法）
+//                     * @param proxy 代理对象的引用（一般不用）
+//                     * @param method 当前执行的方法
+//                     * @param args 当前执行方法所需的参数
+//                     * @return 返回代理后的对象
+//                     * @throws Throwable
+//                     */
+//                    @Override
+//                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//                        //声明切面
+//                        MyAspect myAspect = new MyAspect();
+//                        //前增强
+//                        myAspect.check_Permission();
+//                        //在目标类上调用方法，并传入参数
+//                        Object obj = method.invoke(userDao, args);
+//                        //后增强
+//                        myAspect.log();
+//                        return obj;
+//                    }
+//                });
     }
 
     /**
      * 所有动态代理类的方法调用，都会交由invoke()方法去处理
-     * @param proxy 被代理后的对象
-     * @param method 将要被执行的方法信息（反射）
+     *
+     * @param proxy   被代理后的对象
+     * @param method  将要被执行的方法信息（反射）
      * @param objects 执行方法时需要的参数
      */
     @Override
