f2217ed2a6dc3d7ac0790c94c92a1626d96c0bfb
WZZ <1974077165@qq.com>
2020-05-08 12:10:45 +0800

    学习SpringMVC上传文件的相关内容

---
+++ chapter16/src/main/java/cn/sdut/controller/UserController.java
@@ -0,0 +1,35 @@
+package cn.sdut.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("/fileUpload")
+    public String fileUpload1(HttpServletRequest request, MultipartFile upload) throws IOException {
+        System.out.println("SpringMVC 文件上传...");
+        // 获取要上传的文件目录
+        String path = request.getSession().getServletContext().getRealPath("/uploads");
+        // 创建File对象，向该路径上传文件
+        File file = new File(path);
+        // 判断路径是否存在，如不存在则创建
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        // 获取上传文件的名称
+        String filename = upload.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        // 把文件名称唯一化
+        filename = uuid + "_" + filename;
+        // 上传文件
+        upload.transferTo(new File(file, filename));
+        return "success";
+    }
+}
---
+++ chapter16/src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 19740
+  Date: 2020/5/7
+  Time: 下午 5:56
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>文件上传</h1>
+<form action="user/fileUpload" method="post" enctype="multipart/form-data">
+    选择文件：<input type="file" name="upload"/><br>
+    <input type="submit" value="上传" />
+</form>
+</body>
+</html>
